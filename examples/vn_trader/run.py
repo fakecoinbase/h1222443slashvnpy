e17730cbcd443e22ef7973954363fbbf1b0a20ae
Administrator <857954176@qq.com>
2020-06-18 15:27:22 +0800

    Revert "dad"
    This reverts commit a73d8ac1

--- examples/vn_trader/run.py
+++ examples/vn_trader/run.py
@@ -37,11 +37,11 @@ from vnpy.gateway.ctp import CtpGateway
 # from vnpy.gateway.okexo import OkexoGateway
 # from vnpy.gateway.binancef import BinancefGateway
 
-# from vnpy.app.cta_strategy import CtaStrategyApp
+from vnpy.app.cta_strategy import CtaStrategyApp
 # from vnpy.app.csv_loader import CsvLoaderApp
 # from vnpy.app.algo_trading import AlgoTradingApp
-# from vnpy.app.cta_backtester import CtaBacktesterApp
-# from vnpy.app.data_recorder import DataRecorderApp
+from vnpy.app.cta_backtester import CtaBacktesterApp
+from vnpy.app.data_recorder import DataRecorderApp
 # from vnpy.app.risk_manager import RiskManagerApp
 # from vnpy.app.script_trader import ScriptTraderApp
 # from vnpy.app.rpc_service import RpcServiceApp
@@ -95,11 +95,11 @@ def main():
     # main_engine.add_gateway(OkexoGateway)
     # main_engine.add_gateway(BinancefGateway)
 
-    # main_engine.add_app(CtaStrategyApp)
-    # main_engine.add_app(CtaBacktesterApp)
+    main_engine.add_app(CtaStrategyApp)
+    main_engine.add_app(CtaBacktesterApp)
     # main_engine.add_app(CsvLoaderApp)
     # main_engine.add_app(AlgoTradingApp)
-    # main_engine.add_app(DataRecorderApp)
+    main_engine.add_app(DataRecorderApp)
     # main_engine.add_app(RiskManagerApp)
     # main_engine.add_app(ScriptTraderApp)
     # main_engine.add_app(RpcServiceApp)
